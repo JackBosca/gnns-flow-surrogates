2c7c5ac34476cfb21a552794e22cfa3abb366b57
JackBosca <boscarioljacopo@gmail.com>
2025-09-22 20:20:16 +0200

    release Normalizer class

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,5 @@
+import torch
+import torch.nn as nn
 from torch_geometric.data import Data
 
 # def decompose_graph(graph):
@@ -41,3 +43,70 @@ def copy_geometric_data(graph):
     g_copy.global_attr = global_attr
 
     return g_copy
+
+class Normalizer(nn.Module):
+    '''
+    Normalizes input data and accumulates statistics (mean, std) during training.
+    '''
+    def __init__(self, size, max_accumulations=10**6, std_epsilon=1e-8, name='Normalizer', device='cuda'):
+        super(Normalizer, self).__init__()
+
+        self.name=name
+        # max number of batch to accumulate statistics for, to prevent overflow
+        self._max_accumulations = max_accumulations
+        # constant to prevent division by zero
+        self._std_epsilon = torch.tensor(std_epsilon, dtype=torch.float32, requires_grad=False, device=device)
+        # total number of samples accumulated so far
+        self._acc_count = torch.tensor(0, dtype=torch.float32, requires_grad=False, device=device)
+        # how many batches have been accumulated so far
+        self._num_accumulations = torch.tensor(0, dtype=torch.float32, requires_grad=False, device=device)
+        # sum and square sum for mean and std computation
+        self._acc_sum = torch.zeros((1, size), dtype=torch.float32, requires_grad=False, device=device)
+        self._acc_sum_squared = torch.zeros((1, size), dtype=torch.float32, requires_grad=False, device=device)
+
+    def forward(self, batch_data, accumulate=True):
+        """Normalize input data and accumulate statistics."""
+        if accumulate:
+        # stop accumulating after a million updates, to prevent accuracy issues
+            if self._num_accumulations < self._max_accumulations:
+                # detach data so that accumulating does not influence gradients
+                self._accumulate(batch_data.detach())
+        return (batch_data - self._mean()) / self._std_with_epsilon()
+
+    def inverse(self, normalized_batch_data):
+        """Inverse transformation of the normalizer."""
+        return normalized_batch_data * self._std_with_epsilon() + self._mean()
+
+    def _accumulate(self, batch_data):
+        """Function to perform the accumulation of the batch_data statistics."""
+        count = batch_data.shape[0]
+        data_sum = torch.sum(batch_data, axis=0, keepdims=True)
+        squared_data_sum = torch.sum(batch_data**2, axis=0, keepdims=True)
+
+        # update accumulators counts
+        self._acc_sum += data_sum
+        self._acc_sum_squared += squared_data_sum
+        self._acc_count += count
+        self._num_accumulations += 1
+
+    def _mean(self):
+        safe_count = torch.maximum(self._acc_count, torch.tensor(1.0, dtype=torch.float32, device=self._acc_count.device))
+        return self._acc_sum / safe_count
+
+    def _std_with_epsilon(self):
+        safe_count = torch.maximum(self._acc_count, torch.tensor(1.0, dtype=torch.float32, device=self._acc_count.device))
+        std = torch.sqrt(self._acc_sum_squared / safe_count - self._mean()**2)
+        return torch.maximum(std, self._std_epsilon)
+
+    def get_variable(self):
+        dict = {'_max_accumulations':self._max_accumulations,
+        '_std_epsilon':self._std_epsilon,
+        '_acc_count': self._acc_count,
+        '_num_accumulations':self._num_accumulations,
+        '_acc_sum': self._acc_sum,
+        '_acc_sum_squared':self._acc_sum_squared,
+        'name':self.name
+        }
+
+        return dict
+    
