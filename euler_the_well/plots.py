9734545a0fc073db7a75774ee37d20c625623e57
JackBosca <boscarioljacopo@gmail.com>
2025-11-17 12:16:26 +0100

    add rolling average to improve plot readibility

--- euler_the_well/plots.py
+++ euler_the_well/plots.py
@@ -5,7 +5,7 @@ import os
 import glob
 
 
-def plot_losses(losses_path='checkpoint', save_path="loss_plot.png", show=False, log_scale=False):
+def plot_losses(losses_path='checkpoint', save_path="loss_plot.png", show=False, log_scale=False, smooth=0):
     # load and concatenate all losses_epoch_*.pt
     pattern = os.path.join(losses_path, "losses_epoch_*.pt")
     files = sorted(glob.glob(pattern))
@@ -19,6 +19,13 @@ def plot_losses(losses_path='checkpoint', save_path="loss_plot.png", show=False,
         all_losses.extend(epoch_losses.tolist())
 
     losses = torch.tensor(all_losses)
+
+    # optional smoothing with rolling average
+    if smooth > 1:
+        kernel = torch.ones(smooth) / smooth
+        losses = torch.nn.functional.conv1d(
+            losses.view(1,1,-1), kernel.view(1,1,-1), padding=smooth//2
+        ).view(-1)
 
     plt.figure(figsize=(10, 5))
     plt.plot(losses, label='Training Loss')
@@ -43,6 +50,9 @@ if __name__ == '__main__':
     parser.add_argument('--save-path', type=str, default='loss_plot.png', help='Path to save the loss plot image')
     parser.add_argument('--show', action='store_true', help='Whether to display the plot interactively')
     parser.add_argument('--log-scale', action='store_true', help='Whether to use logarithmic scale for y-axis')
+    parser.add_argument('--smooth', type=int, default=0, help='Rolling average window size; 0=disabled')
+
     args = parser.parse_args()
 
-    plot_losses(args.losses_path, args.save_path, args.show, args.log_scale)
+    plot_losses(args.losses_path, args.save_path, args.show, args.log_scale, args.smooth)
+    
